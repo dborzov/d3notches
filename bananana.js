6e3354b2d77a60b495ec605414d39cf17d1cd08d
Peter Borzov <peter@sowingo.com>
2014-02-22 21:13:29 -0500

    Basic plot

--- bananana.js
+++ bananana.js
@@ -1,3 +1,15 @@
 $(function() {
-    console.log( "Js represents!" );
+    var data = [234, 345, 470];
+    var x = d3.scale.linear()
+        .domain([0, d3.max(data)])
+        .range([0,420]);
+
+    d3.select(".chart")
+        .selectAll("div")
+        .data(data)
+        .enter()
+        .append("div")
+        .style("width", function(d) { return x(d) + "px";})
+        .text(function(d) {return d;});
+
 });
