49e364156e020b4d0fc190c5f8d085bea07bc9c4
Peter Borzov <peter@sowingo.com>
2014-02-23 01:17:28 -0500

    Action button

--- bananana.js
+++ bananana.js
@@ -9,7 +9,15 @@ $(function() {
         .data(data)
         .enter()
         .append("div")
-        .style("width", function(d) { return x(d) + "px";})
+        .style("width", function(d) {
+         return x(d) + "px";
+        })
         .text(function(d) {return d;});
+
+    $('#actionButton').click(function() {
+        bars =  d3.select(".chart").selectAll("div");
+        bars.style('width','100px');
+        console.log('Button handler loves you');
+    });
 
 });
